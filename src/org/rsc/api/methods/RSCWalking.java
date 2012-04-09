61be8c43ec3938e24f0112a1abe580f4eb2df790
Xel <xelji@hotmail.com>
2012-04-09 06:23:29 +0300

    Initial

---
+++ src/org/rsc/api/methods/RSCWalking.java
@@ -0,0 +1,5 @@
+package org.rsc.api.methods;
+
+public class RSCWalking {
+
+}
---
+++ src/org/rsc/client/Private.java
@@ -0,0 +1,11 @@
+package org.rsc.client;
+
+
+/**
+ * all manip methods private for now
+ * @author Xel
+ *
+ */
+public class Private {
+
+}
